be5c56fe0fca98c852b29fa9aae950326ce4f592
Michael Kucinski <engineermichigan@gmail.com>
2015-07-02 16:44:15 -0700

    Added second TableViewController

---
+++ PixoThreeTabViewXcodeProject/PixoThreeTabViewXcodeProject/PMUserListTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  PMUserListTableViewController.h
+//  PixoThreeTabViewXcodeProject
+//
+//  Created by Michael Kucinski on 7/2/15.
+//  Copyright (c) 2015 Michael Kucinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMUserListTableViewController : UITableViewController
+@property (nonatomic, strong) NSDictionary *users;
+
+@end
---
+++ PixoThreeTabViewXcodeProject/PixoThreeTabViewXcodeProject/PMUserListTableViewController.m
@@ -0,0 +1,123 @@
+//
+//  PMUserListTableViewController.m
+//  PixoThreeTabViewXcodeProject
+//
+//  Created by Michael Kucinski on 7/2/15.
+//  Copyright (c) 2015 Michael Kucinski. All rights reserved.
+//
+
+#import "PMUserListTableViewController.h"
+
+@interface PMUserListTableViewController ()
+
+@end
+
+@implementation PMUserListTableViewController
+
+@synthesize users;
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+
+    NSString *file = [[NSBundle mainBundle] pathForResource:@"MockupUsers" ofType:@"plist"];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if ([fileManager fileExistsAtPath:file]) {
+        NSLog(@"The file exists");
+    } else {
+        NSLog(@"The file does not exist");
+    }
+    
+    users = [[NSDictionary alloc] initWithContentsOfFile:file];
+    NSLog(@"%i", [users count]);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+
+    int numOfRows = [users count];
+    return numOfRows;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    NSArray * values = [users allValues];
+    cell.textLabel.text = [values objectAtIndex:indexPath.row];
+    //    cell.textLabel.text = @"Test";
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
