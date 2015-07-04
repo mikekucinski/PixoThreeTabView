9d127b97b3bc5daee59996896d36ea3863096962
Michael Kucinski <engineermichigan@gmail.com>
2015-07-04 10:28:17 -0700

    clone

---
+++ PixoThreeTabViewXcodeProject/PixoThreeTabViewXcodeProject/PMContactSearchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PMContactSearchTableViewCell.h
+//  PixoThreeTabViewXcodeProject
+//
+//  Created by Michael Kucinski on 7/3/15.
+//  Copyright (c) 2015 Michael Kucinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMContactSearchTableViewCell : UITableViewCell
+
+@end
---
+++ PixoThreeTabViewXcodeProject/PixoThreeTabViewXcodeProject/PMContactSearchTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PMContactSearchTableViewCell.m
+//  PixoThreeTabViewXcodeProject
+//
+//  Created by Michael Kucinski on 7/3/15.
+//  Copyright (c) 2015 Michael Kucinski. All rights reserved.
+//
+
+#import "PMContactSearchTableViewCell.h"
+
+@implementation PMContactSearchTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
